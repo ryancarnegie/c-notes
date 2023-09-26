8776979afde8c7a6375750851d7648c22982d9ee
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2023-09-25 18:05:32 -0700

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -10,55 +10,45 @@ double conversion(double number1 / double number2);
 const float EXIT_PROGRAM = -1.0;
 const float MINIMUM_VALUE = 0.1;
 
-int main() // begin main
-{
+int main() // begin main {
 	double gallons = 0; // gallons entered by user
 	double miles   = 0; // miles entered by user
 	double sumGall = 0; // sum the total gallons entered
 	double sumMile = 0; // sum the total miles entered
 
-	do
-	{
+	do {
 		printf("Enter gallons used: (-1.0 to end): "); // user enters an amount in gallons
 		scanf("%lf", &gallons); // scan the value
-		while (gallons < MINIMUM_VALUE && gallons != EXIT_PROGRAM) // condition for invalid cases
-		{
+		while (gallons < MINIMUM_VALUE && gallons != EXIT_PROGRAM) { // condition for invalid cases 
 			printf("You have entered an invalid value for gallons, try again: ");
 			scanf("%lf", &gallons);
 		}		
-		if (gallons > MINIMUM_VALUE)
-		{
+		if (gallons > MINIMUM_VALUE) {
 				sumGall = sumGall + gallons; // if valid cases, add gallons to a total sum
 		}
 
-		if (gallons != EXIT_PROGRAM)
-		{
+		if (gallons != EXIT_PROGRAM) {
 			printf("Enter miles used: "); // enter the miles
 			scanf("%lf", &miles);
-			while (miles < MINIMUM_VALUE && miles != EXIT_PROGRAM) // condition for invalid cases
-			{
+			while (miles < MINIMUM_VALUE && miles != EXIT_PROGRAM) { // condition for invalid cases 
 				printf("You have entered an invalid value for miles, try again: ");
 				scanf("%lf", &miles);
 				
 			}	
-			if (miles > MINIMUM_VALUE)
-			{
+			if (miles > MINIMUM_VALUE) {
 				sumMile = sumMile + miles; // if valid casdes, add miles to total miles sum
 			}
 		}
 
-		if (gallons != EXIT_PROGRAM && miles != EXIT_PROGRAM) // if condition is not -1, convert miles / gallons 
-		{
+		if (gallons != EXIT_PROGRAM && miles != EXIT_PROGRAM) { // if condition is not -1, convert miles / gallons 
 			printf("\nThe miles / gallons for this tank was %.6f\n\n", conversion(miles, gallons));
 		}
 	} while (gallons != EXIT_PROGRAM && miles != EXIT_PROGRAM); // end do while loop condition
 
-	if (sumGall > 0) // checks for 0 condition
-	{
+	if (sumGall > 0) // checks for 0 condition {
 		printf("\nThe overall average miles / gallons is: %.6f\n", conversion(sumMile, sumGall));
 	}
-	else
-	{
+	else {
 		printf("The overall average miles could not be completed ;-(\n"); // sad face if NaN is found, as in divide by zero
 	}
 	printf("\nThat's all folks - press any key to exit\n");
