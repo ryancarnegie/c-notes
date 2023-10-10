bf0c5035ae9c71e2d12cc7da6597a9e4e42264ea
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2023-10-09 17:33:53 -0700

    Update Maximum.c

--- Maximum.c
+++ Maximum.c
@@ -4,6 +4,10 @@
 int maximum(int x, int y, int z); // header prototype
 
 int main(void) {
+    int number1 = 0; // first integer entered by the user
+    int number2 = 0; // second integer
+    int number3 = 0; // third integer
+	
     printf("%s", "Enter three integers: ");
        scanf("%d%d%d", &number1, &number2, &number3);
     printf("Maximum is: %d\n", maximum(number1, number2, number3));
