6be154ac09e27ab4224f49822c957c6a8ba02b93
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2023-12-05 17:52:54 -0800

    Update ReverseString.c

--- ReverseString.c
+++ ReverseString.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 #define STRING 15
-#define MAX_STIRNG 20
+#define MAX_STRING_LENGTH 20
 
 int main(int argc, char *argv[]) {
 
-	char string[MAX_STIRNG] = "this is a string\n";
+	char string[MAX_STRING_LENGTH] = "this is a string\n";
 	for (int i = STRING; string[i] >= STRING; i--) {
 		printf("%c", string[i]);
 	}
